9246d199c5b6249a3986ea96d081fe34f35f43a2
freakpirate <shubham.sahu485@gmail.com>
2016-06-30 11:53:35 +0530

    ApiDoc: version 0.2.0 released with new methods and database changes

--- api_project.js
+++ api_project.js
@@ -1,9 +1,9 @@
 define({
   "name": "God API",
-  "version": "0.0.0",
+  "version": "0.2.0",
   "description": "God API- Robust API for wallpapers",
   "title": "God API Documentation",
-  "url": "http://freakpirate.ddns.net:8080/v1.0",
+  "url": "http://freakpirate.ddns.net:8080/v2",
   "header": {
     "title": "Overview",
     "content": "<h4>Welcome to God API</h4>\n<p>This is a robust API which provide wallpapers and/or images of Indian Deities. (Currently in development phase)</p>\n<p>God API is primarily developed for the purpose of harnessing its power in an android application <a href=\"https://github.com/FreakPirate/wallpaper_app\">GodWall</a>. This API provides wallpapers in multiple aspect ratios depending on one's need. <code>Source code</code> of API is available at <a href=\"https://github.com/FreakPirate/god_api\">GitHub</a>.</p>\n<p>Sorting order implemented in the API are:</p>\n<ul>\n<li>Top Rated</li>\n<li>Popularity</li>\n<li>Random</li>\n<li>Category</li>\n<li>ID</li>\n</ul>\n<p>Frameworks used:</p>\n<ul>\n<li><code>Flask</code></li>\n<li><code>SQLite</code></li>\n<li><code>Python</code></li>\n</ul>\n<p>Libraries used:</p>\n<ul>\n<li><code>flask-restful</code></li>\n<li><code>sqlalchemy</code></li>\n<li><code>json</code></li>\n</ul>\n"
@@ -19,7 +19,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2016-06-23T17:04:24.512Z",
+    "time": "2016-06-30T06:08:04.973Z",
     "url": "http://apidocjs.com",
     "version": "0.16.1"
   }
