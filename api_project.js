c2c122948cf03dd9619cfabe81d2f47df96b8f7e
freakpirate <shubham.sahu485@gmail.com>
2016-07-07 11:21:44 +0530

    New method /postWallpaper added

--- api_project.js
+++ api_project.js
@@ -3,7 +3,7 @@ define({
   "version": "0.3.0",
   "description": "God API- Robust API for wallpapers",
   "title": "God API Documentation",
-  "url": "http://freakpirate.ddns.net:8080/v2",
+  "url": "http://freakpirate1.pythonanywhere.com/v3",
   "header": {
     "title": "Overview",
     "content": "<h4>Welcome to God API</h4>\n<p>This is a robust API which provide wallpapers and/or images of Indian Deities. (Currently in development phase)</p>\n<p>God API is primarily developed for the purpose of harnessing its power in an android application <a href=\"https://github.com/FreakPirate/wallpaper_app\">GodWall</a>. This API provides wallpapers in multiple aspect ratios depending on one's need. <code>Source code</code> of API is available at <a href=\"https://github.com/FreakPirate/god_api\">GitHub</a>.</p>\n<p>Sorting order implemented in the API are:</p>\n<ul>\n<li>Top Rated</li>\n<li>Popularity</li>\n<li>Random</li>\n<li>Category</li>\n<li>ID</li>\n</ul>\n<p>Frameworks used:</p>\n<ul>\n<li><code>Flask</code></li>\n<li><code>SQLite</code></li>\n<li><code>Python</code></li>\n</ul>\n<p>Libraries used:</p>\n<ul>\n<li><code>flask-restful</code></li>\n<li><code>sqlalchemy</code></li>\n<li><code>json</code></li>\n</ul>\n"
@@ -13,13 +13,14 @@ define({
     "content": "<h4>Epilogue</h4>\n<p>Suggestions, contact, support and error reporting on <a href=\"https://github.com/FreakPirate/god_api\">GitHub</a>, or contact us via e-mail @ &quot;shubham.sahu485@gmail.com&quot;.</p>\n<h4>Developers</h4>\n<ul>\n<li><a href=\"https://github.com/FreakPirate\">freakpirate aka Shubham Sahu</a></li>\n<li><a href=\"https://github.com/pareshchouhan\">Paresh Chouhan</a></li>\n</ul>\n"
   },
   "template": {
-    "withGenerator": true
+    "withGenerator": true,
+    "withCompare": true
   },
   "sampleUrl": false,
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2016-06-30T12:30:31.926Z",
+    "time": "2016-07-07T05:29:16.513Z",
     "url": "http://apidocjs.com",
     "version": "0.16.1"
   }
